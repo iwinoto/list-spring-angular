845ccbb23e683e19f6d3ecb82eb5780f9e0474e7
Iwan Winoto <iwinoto@gmail.com>
2015-10-29 00:30:35 +1100

    converted to war package and run in Liberty

--- src/main/java/be/g00glen00b/Application.java
+++ src/main/java/be/g00glen00b/Application.java
@@ -1,13 +1,19 @@
 package be.g00glen00b;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
